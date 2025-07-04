604a63776e5ef49e326f7f7ae67df89490580aff
ibrahim çıngı <ibrahimcingi@ibrahim-Dizustu-Bilgisayar.local>
2025-07-04 12:58:44 +0300

    CORSMiddleware imported and implemented

--- app/main.py
+++ app/main.py
@@ -9,11 +9,22 @@ from . import models,schemas,utils
 from .database import get_db,engine
 from sqlalchemy.orm import Session
 from .routers import users,posts,auth,likes
+from fastapi.middleware.cors import CORSMiddleware
 
 
 #models.Base.metadata.create_all(bind=engine)
 
 app=FastAPI()
+
+origins=["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
